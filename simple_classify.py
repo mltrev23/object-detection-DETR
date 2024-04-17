d224532f6c772a5d64195459fcfee2f4e165841d
mltrev23 <trevor.dev23@gmail.com>
2024-04-17 08:24:09 -0500

    show results and save

--- simple_classify.py
+++ simple_classify.py
@@ -1,10 +1,10 @@
 from transformers import DetrImageProcessor, DetrForObjectDetection
 import torch
-from PIL import Image
+from PIL import Image, ImageDraw
 import requests
 from io import BytesIO
 import matplotlib.pyplot as plt
-
+import matplotlib
 #url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 
 #response = requests.get(url)
@@ -14,9 +14,9 @@ import matplotlib.pyplot as plt
 #image = Image.open(BytesIO(response.content))
 image = Image.open('test.jpg')
 #image.show()  # This will open the image using an image viewer
-plt.imshow(image)
-plt.axis('off')
-plt.show()
+#plt.imshow(image)
+#plt.axis('off')
+#plt.show()
 
 # you can specify the revision tag if you don't want the timm dependency
 processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
@@ -30,9 +30,17 @@ outputs = model(**inputs)
 target_sizes = torch.tensor([image.size[::-1]])
 results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
 
+draw = ImageDraw.Draw(image)
+
 for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
     box = [round(i, 2) for i in box.tolist()]
     print(
-            f"Detected {model.config.id2label[label.item()]} with confidence "
-            f"{round(score.item(), 3)} at location {box}"
+		f"Detected {model.config.id2label[label.item()]} with confidence "
+		f"{round(score.item(), 3)} at location {box}"
     )
+    top_left_corner = (box[0], box[1])  # x, y of the top left corner
+    bottom_right_corner = (box[2], box[3])  # x, y of the bottom right corner
+    draw.rectangle([top_left_corner, bottom_right_corner], outline="red", width=3)
+
+image.save('out.jpg')
+#image.show()
